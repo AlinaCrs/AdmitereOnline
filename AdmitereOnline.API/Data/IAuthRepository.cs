2e51452c084ca05e91ce2003d0499b57ff50d4ec
Alina Cristea <alina.crs77@gmail.com>
2020-06-24 00:33:30 +0300

    End of section 3

---
+++ AdmitereOnline.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using AdmitereOnline.API.Models;
+
+namespace AdmitereOnline.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExist(string username);
+    }
+}
