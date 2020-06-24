92ca3749a36dfb961530e6e98ac70907f78cc7a0
Alina Cristea <alina.crs77@gmail.com>
2020-06-24 05:06:53 +0300

    end of 4

--- AdmitereOnline.API/Startup.cs
+++ AdmitereOnline.API/Startup.cs
@@ -33,19 +33,8 @@ namespace AdmitereOnline.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                     .AddJwtBearer(options => {
-                         options.TokenValidationParameters = new TokenValidationParameters 
-                         {
-                             ValidateIssuerSigningKey = true,
-                            
-                             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                             
-                            ValidateIssuer = false,
-                            ValidateAudience = false,
-                         };
-                     });
+            services.AddScoped<IAuthRepository, AuthRepository>();                          
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
